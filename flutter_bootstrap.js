46385f4476b4f885d57e3f9b0d3fbc6e56ef87d3
Soffia Sanchezz <ssofiiadayanna@gmail.com>
2025-06-26 11:27:43 -0500

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3680137235"
+    serviceWorkerVersion: "2956133063"
   }
 });
